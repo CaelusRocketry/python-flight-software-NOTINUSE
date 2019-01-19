6493fcfe9df18a76c14453f567ad43e995e1ac5a
Stephen Huan <stephen03.huan@gmail.com>
2019-01-19 17:42:04 -0500

    Change variables

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import yaml
 import os
 
-config = None  # Initialize config
 submodules = None
 
 def load_config():
@@ -10,26 +9,18 @@ def load_config():
     If *config_custom.yaml* exists, use that (this is user-configurable).
     Else, use *config_default.yaml*. This should not be changed while testing.
     """
-    global config
 
     config_filename = "config_default.yaml"
     if os.path.exists("config_custom.yaml"):
         config_filename = "config_custom.yaml"
     with open(config_filename, 'r') as stream:
         try:
-            config = yaml.load(stream)
+             return yaml.load(stream)
         except yaml.YAMLError as e:
             print(e)
 
 def load_submodules():
     pass
 
-
-def start():
-    load_config()
-    load_submodules()
-
-    print(config)
-
 if __name__ == "__main__":
-    start()
+    config = load_config()
