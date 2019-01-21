0da54dbd2cf9e6bde014982508e1fce1bb24af98
Srikar Gouru <srikarg89@gmail.com>
2019-01-20 23:22:31 -0500

    Use multiple sockets when communicating with ground

--- modules/telemetry.py
+++ modules/telemetry.py
@@ -7,9 +7,21 @@ from . import tvc
 from main import load_config
 
 messageQueue = []
-
+BUFFER = 1024
 config = load_config()
 
+def createReceivingSocket(MY_IP, RECV_PORT):
+    """
+    Creates and connects a socket used for receiving.
+    An error will occur  after some time if there is no response from the ground station.
+    :param MY_IP: IP of the raspberry PI.
+    :param RECV_PORT: Port meant to be used for receiving socket.
+    """
+    global sock_receive, conn_receive
+    sock_receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_receive.bind((MY_IP, RECV_PORT))
+    sock_receive.listen(1)
+    conn_receive, addr = sock_receive.accept()
 
 def enqueue(message, timestamp, priority=1):
     """
@@ -18,52 +30,72 @@ def enqueue(message, timestamp, priority=1):
     :param timestamp: time when message was conceived
     :param priority: priority status of message - default is 1, emergency is 0
     """
-    global messageQueue
     heapq.heappush(messageQueue, (priority, message, timestamp))
 
 
 def send():
     """
     Loops through the messages in the queue and sends them to ground using sockets.
+    Delay of 50ms between each send.
     """
+    global sock_send
 
     while True:
-        while len(messageQueue) > 0:
+        if len(messageQueue) > 0:
             priority, message, timestamp = heapq.heappop(messageQueue)
-            # TODO: Actually send the message using sockets
+            message = message.encode('utf-8')
+            sock_send.sendall(message + '\r')
             print(message + "\t" + "Time: " + timestamp)
+        time.sleep(50.0/1000.0)
 
-
-def t_connect(HostIp, Port):
-    global s
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HostIp, Port))
-    return
-
-
-def t_write(D):
-    s.send(D + '\r')
-    return
-
+def t_connect(groundstationIP, sendPort):
+    """
+    Creates and connects socket used for sending with the given IP and port.
+    Assumes the ground station receiving socket is attempting to connect.
+    :param groundstationIP: IP address of the ground station.
+    :param sendPort: Port meant to be used for the sending socket.
+    """
+    global sock_send
+    sock_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_send.connect((groundstationIP, sendPort))
 
 def t_read():
-    a = ""
-    while a == "":
-        a = s.recv(1)
+    """
+    Reads the next message from the ground or stalls until a message is available.
+    BUFFER value can be changed by other methods.
+    :return a: decoded string message that was received.
+    """
+    global conn_receive
+    a = conn_receive.recv(BUFFER)
+    a = a.decode('utf-8')
     return a
 
-
-def t_close():
-    s.close()
-    return
-
-
 def listen():
-    if t_read() == "start tvc":
-        tvc.stabilize()
+    global sock_receive, conn_receive
+    while 1:
+        data = t_read()
+        if data == "END CONNECTION":
+            break
+        messageDigest(data)
 
+    conn_receive.close()
+    sock_receive.close()
+
+def messageDigest(message):
+    """
+    Reads message header and calls appropriate methods based on that.
+    :param message:
+    """
+    if message == "CONNECT SENDING SOCKET":
+        t_connect(config["modules"]["telemetry"]["groundstationIP"], config["modules"]["telemetry"]["sendPort"])
+        # TODO: Configure Ground Station IP and Port in config
+        enqueue('We are connected!')
 
 def start():
-    t_connect(config["modules"]["telemetry"]["hostIP"], config["modules"]["telemetry"]["port"])
-    # TODO: Configure HostIP and Port in config
-    t_write('We are connected!')
+    """
+    Creates and starts a thread for the receiving socket.
+    """
+    # TODO: Configure PI IP and Port in config
+    createReceivingSocket(config["modules"]["telemetry"]["piIP"], config["modules"]["telemetry"]["receivePort"])
+    recv_thread = Thread(target=listen)
+    recv_thread.start()
