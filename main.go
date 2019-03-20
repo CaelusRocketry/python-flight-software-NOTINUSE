ef720f14fdcc7e98824aad6fa03a0d105c1c9126
Akash Bhave <akashbbhave@gmail.com>
2019-03-19 20:11:13 -0400

    Convert os.File to byte array in main.loadConfig()
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- main.go
+++ main.go
@@ -2,24 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
-func loadConfig() *os.File {
-	var file *os.File
+func loadConfig() {
+	var configFile *os.File
 	if _, err := os.Stat("config_custom.yml"); err != nil && os.IsNotExist(err) {
-		file, err = os.Open("config_default.yml")
+		configFile, err = os.Open("config_default.yml")
 		if err != nil {
-			fmt.Println("Bad")
+			fmt.Println("Error occurred with reading 'config_default.yml'")
 		}
 	} else {
-		file, _ = os.Open("config_custom.yml")
+		configFile, _ = os.Open("config_custom.yml")
 	}
 
-	return file
+	fmt.Println("Config filename:", configFile.Name())
+
+	configBytes, _ := ioutil.ReadAll(configFile)
+	fmt.Println(string(configBytes))
 }
 
 func main() {
-	config := loadConfig()
-	fmt.Println(config.Name())
+	loadConfig()
 }
