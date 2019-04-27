c7aa96d2b4beae315780e6c0fb5c0d41f8fa0ed4
Akash Bhave <akashbbhave@gmail.com>
2019-04-27 18:11:36 -0400

    Move code in modules/supervisordraft to modules/supervisor
    Signed-off-by: Akash Bhave <akashbbhave@gmail.com>

--- modules/sensors/main.go
+++ modules/sensors/main.go
@@ -4,6 +4,13 @@ import (
 	"fmt"
 )
 
+// Sensor represents a sensor.
+type Sensor struct {
+	critLow, low, high, critHigh float64
+	isCorrecting                 bool
+}
+
+// Pitch = nil.
 func Pitch() {
 	fmt.Println("PITCH")
 }
