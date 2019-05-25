c98c916a73961c57f8c557d700afb01a192c68fc
Akash Bhave <akashbbhave@gmail.com>
2019-05-25 18:55:20 -0400

    Fix various errors

--- modules/sensors/imu.go
+++ modules/sensors/imu.go
@@ -33,7 +33,7 @@ func InitIMU() *IMU {
 		panic(err)
 	}
 
-	defer b.Close()
+	// defer b.Close()
 
 	dev, err := bno055.NewI2C(b, 0x28)
 
@@ -84,7 +84,7 @@ func (s *IMU) AccZ() float64 {
 // Gyro returns a vector with gyro data
 func (s *IMU) Gyro() []float64 {
 	gyroVector, err := s.dev.GetVector(VectorGyroscope)
-	for i, val := range gyroVector {
+	for i, _ := range gyroVector {
 		gyroVector[i] = gyroVector[i] * 180 / math.Pi
 	}
 	if err != nil {
