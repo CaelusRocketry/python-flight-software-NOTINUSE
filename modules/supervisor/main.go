faed7ba3ba033fba187584a3795502b2e70d30f2
Srikar Gouru <srikarg89@gmail.com>
2019-06-01 19:29:55 -0400

    hi

--- modules/supervisor/main.go
+++ modules/supervisor/main.go
@@ -1,41 +1,37 @@
 package supervisor
 
 import "time"
+import sensor "flight-software/modules/sensors"
 
-var delay = 0.01
+var delay = 0.1
 
 func Start() {
 	iters := 0
 	start := time.Now()
 
-	allSensors := 
+	sensors := []sensor.Sensor
+	sensors = append(sensors, sensors.InitIMU())
 
-	sensors := []SensorObj
-
-	for {
+	for i := 0; i < 50; i++{
 		for time.Since(start)-iters*delay < 0 {
 			//wait until 200 nanoseconds have passed
 		}
 		var data = make(map[string]float64)
 		for curr := range sensors {
-			sensorname := curr.name
+			sensorname := curr.Name()
 			//var reading = sensor.getData()
 			reading := curr.GetData()
 			data[sensorname] = reading
-			if !curr.corrected && (reading < curr.low || reading > curr.high) {
-				curr.correcting = true
-				go curr.correct()
+//			if !curr.corrected && (reading < curr.low || reading > curr.high) {
+//				curr.correcting = true
+//				go curr.correct()
 				//determine if correction is critical or not,
 				//do some correction in seperate thread,
 				//flag correction once done
-			}
+//			}
 		}
-		telemetrypush(time.Now().Sub(start), data)
+//		telemetrypush(time.Now().Sub(start), data)
+		fmt.Println(data)
 		iters++
-
 	}
-}
-
-func (s *SensorObj) GetData() {
-
 }
