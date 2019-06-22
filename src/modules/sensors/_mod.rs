5aabf932abfd91d1d896b2437e6949e46fe7587d
Akash Bhave <akashbbhave@gmail.com>
2019-06-22 19:07:19 -0400

    Create sensor trait, test impl functionality

--- src/modules/sensors/_mod.rs
+++ src/modules/sensors/_mod.rs
@@ -1,1 +1,12 @@
 pub mod imu;
+
+pub struct Sensor {
+    pub name: String,
+    pub safe_val: i16,
+    pub warn_val: i16,
+    pub crit_val: i16,
+}
+
+pub trait SensorTrait {
+    fn name(&self) -> &String;
+}
--- src/modules/sensors/imu.rs
+++ src/modules/sensors/imu.rs
@@ -1,3 +1,24 @@
+use super::{Sensor, SensorTrait};
+
+pub struct IMU {
+    sensor: Sensor,
+}
+
+impl SensorTrait for IMU {
+    fn name(&self) -> &String {
+        &self.sensor.name
+    }
+}
+
 pub fn imu_test() {
     println!("Testing IMU");
+    let i = IMU {
+        sensor: Sensor {
+            name: String::from("An IMU"),
+            safe_val: 10,
+            warn_val: 15,
+            crit_val: 20,
+        },
+    };
+    println!("{}", i.name());
 }
