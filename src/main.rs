df4d473fe16e99da00ade2c4793da97cd4901eef
Akash Bhave <akashbbhave@gmail.com>
2019-06-25 10:52:36 -0400

    Create supervisor, move startup code into supervisor

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
 #[path = "modules/_mod.rs"]
 mod modules;
 
-use std::thread;
-use std::time::Duration;
-
-use crate::modules::sensors::imu::IMU;
-use crate::modules::sensors::pressure::Pressure;
-use crate::modules::sensors::temperature::Temperature;
-use crate::modules::sensors::SensorTrait;
+use modules::supervisor::main as supervisor;
 
 fn main() {
-    let mut imu = IMU::new("NOSECONE", 0x28);
-    let mut pressure_tank = Pressure::new("TANK", 1.75, 2.0);
-    let mut temp_tank = Temperature::new("TANK", 350.0, 400.0);
-
-    let delay = Duration::from_millis(200);
-
-    println!("Sensor registered: {}", pressure_tank.name());
-    println!("Sensor registered: {}", temp_tank.name());
-    println!("Sensor registered: {}", imu.name());
-
-    loop {
-        println!("{:?}", temp_tank.status());
-        thread::sleep(delay);
-    }
+    supervisor::start();
 }
-
-// fn main() {
-//     modules::telemetry::telem::runsocket();
-// }
--- src/modules/_mod.rs
+++ src/modules/_mod.rs
@@ -1,5 +1,8 @@
 #[path = "sensors/_mod.rs"]
 pub mod sensors;
 
+#[path = "supervisor/_mod.rs"]
+pub mod supervisor;
+
 #[path = "telemetry/_mod.rs"]
 pub mod telemetry;
