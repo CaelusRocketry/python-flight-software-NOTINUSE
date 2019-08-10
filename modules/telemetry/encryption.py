1a6387aaab4c83246bced6470fb41d4c8c0b4505
ayusher <araut@Ayush-Surface.localdomain>
2019-08-10 19:43:18 -0400

    Reformat telemetry folder

---
+++ modules/telemetry/encryption.py
@@ -0,0 +1,26 @@
+import heapq
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import ast  
+import socket, json, time, threading
+import multiprocessing
+import tcp_socket
+
+queue_send=[]
+
+def start():
+    sock = tcp_socket.create_socket()
+    send_thread = threading.Thread(target=tcp_socket.send, args=(sock,))
+    listen_thread = threading.Thread(target=tcp_socket.listen, args=(sock,))
+    send_thread.start()
+    listen_thread.start()
+    print("Listening and sending")
+
+def encode(packet):
+    with open("publickey.txt", "rb") as publickey:
+        return RSA.importKey(publickey.read()).encrypt(packet.encode('utf-8'), 32)
+
+def decode(message):
+    with open("privatekey.txt", "rb") as privatekey:
+        return RSA.importKey(privatekey.read()).decrypt(message).decode("utf-8")
