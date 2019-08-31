b63cd1e6f7bf5e50430fea3aaba518b73151315e
Mitali10 <35309999+Mitali10@users.noreply.github.com>
2019-08-31 17:31:48 -0400

    Add Kalman to Sensor class

--- modules/sensors/__init__.py
+++ modules/sensors/__init__.py
@@ -1,12 +1,16 @@
 from abc import ABC, abstractmethod
-from enum import IntEnum, Enum
+from enum import Enum
 from aenum import auto
 from queue import PriorityQueue
 
-class SensorStatus(IntEnum):
-    Safe = 3
-    Warn = 2
-    Crit = 1
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+import numpy as np
+
+class SensorStatus(Enum):
+    Safe = auto()
+    Warn = auto()
+    Crit = auto()
 
 class SensorType(Enum):
     Temperature = auto()
@@ -39,3 +43,17 @@ class Sensor(ABC):
     @abstractmethod
     def log(cls) -> PriorityQueue:
         pass
+
+    def initKalman(self):
+        self.kf = KalmanFilter( dim_x=2, dim_z=1 )
+        self.kf.x = np.array([[2.], [0.]])
+        self.kf.F = np.array([[1.,1.], [0.,1.]]) 
+        self.kf.H = np.array([[1.,0.]]) 
+        self.kf.P *= 1000.0
+        self.kf.R = 5 
+        self.kf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
+
+    def updateKalman(self, reading):
+        self.kf.predict() 
+        self.kf.update(reading)
+        return kf.x
