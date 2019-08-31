0c30f72813e7d0eeafddb93dddafd958ac1c7ba6
Srikar Gouru <srikarg89@gmail.com>
2019-08-31 16:59:23 -0400

    Valve implementation tested

--- modules/sensors/valve.py
+++ modules/sensors/valve.py
@@ -1,94 +1,103 @@
 import RPi.GPIO as GPIO
 from RPi.GPIO import HIGH, LOW
-import time 
+from time import sleep
+import threading
+import time
 
 GEAR_RATIO = 50
-HALF_STEP = False
-DELAY_TIME = (1/2 if HALF_STEP else 1)*0.001
+DELAY_TIME = .001
 
-out1 = 13
-out2 = 11
-out3 = 15
-out4 = 12
-
-half_step_dict = {
-    -1: [LOW, LOW, LOW, LOW],
-    0: [HIGH, LOW, LOW, LOW],
-    1: [HIGH, HIGH, LOW, LOW],
-    2: [LOW, HIGH, LOW, LOW],
-    3: [LOW, HIGH, HIGH, LOW],
-    4: [LOW, LOW, HIGH, LOW],
-    5: [LOW, LOW, HIGH, HIGH],
-    6: [LOW, LOW, LOW, HIGH],
-    7: [HIGH, LOW, LOW, HIGH]
-}
-
-full_step_dict = {
-    -1: [LOW, LOW, LOW, LOW],
-    0: [HIGH, LOW, LOW, HIGH],
-    1: [LOW, HIGH, LOW, HIGH],
-    2: [LOW, HIGH, HIGH, LOW],
-    3: [HIGH, LOW, HIGH, LOW]
-}
 
 class Valve:
-    
-    def __init__(self, out1, out2, out3, out4):
-        self.out1 = out1
-        self.out2 = out2
-        self.out3 = out3
-        self.out4 = out4
+
+    def __init__(self, dir, step, gear_ratio=GEAR_RATIO, delay=DELAY_TIME):
+        self.is_acuating = False
+        self.gear_ratio = gear_ratio
+        self.dir = dir
+        self.step = step
+        self.active = False
+        self.aborted = False
+        self.delay = delay
         self.angle = 0
-        self.step_num = 0
+        self.full = 200
+        self.priority = -1
+        self.setup()
 
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(out1,GPIO.OUT)
-        GPIO.setup(out2,GPIO.OUT)
-        GPIO.setup(out3,GPIO.OUT)
-        GPIO.setup(out4,GPIO.OUT)
+    def setup(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.dir, GPIO.OUT)
+        GPIO.setup(self.step, GPIO.OUT)
 
-    def step(self, i):
-        stepper_dict = half_step_dict if HALF_STEP else full_step_dict
-        GPIO.output(self.out1,stepper_dict[i][0])
-        GPIO.output(self.out2,stepper_dict[i][1])
-        GPIO.output(self.out3,stepper_dict[i][2])
-        GPIO.output(self.out4,stepper_dict[i][3])
+    def abort(self):
+        GPIO.output(self.step, LOW)
+        GPIO.cleanup()
+        self.aborted = True
 
-#Angle in degrees
-    def setPos(self, target):
-        try:
-            start = time.time()
-            self.step(-1)
-            angle = target - self.angle
-            angle %= 360
-            if angle > 180:
-                angle = angle - 360
-            steps_per_rotation = 400 if HALF_STEP else 200
-            steps = steps_per_rotation * GEAR_RATIO * angle / 360
-            diff = 1
-            if steps < 0:
-                steps = -steps
-                diff = -1
-                
-            print(angle, self.angle, steps*diff)
-            for y in range(steps):
-                self.step(self.step_num)
-                time.sleep(DELAY_TIME)
-                self.step_num += diff
-                if HALF_STEP:
-                    if self.step_num == -1: self.step_num = 7
-                    if self.step_num == 8: self.step_num = 0
-                else:
-                    if self.step_num == -1: self.step_num = 3
-                    if self.step_num == 4: self.step_num = 0
-                if y % steps_per_rotation == 0:
-                    self.step(-1)
-            self.angle = target
-            print("Done")
-        except KeyboardInterrupt:
-            GPIO.cleanup()
+    def _step(self):
+        GPIO.output(self.step, HIGH)
+        sleep(self.delay)
+        GPIO.output(self.step, LOW)
+        sleep(self.delay)
 
-valve = Valve(out1, out2, out3, out4)
-while 1:
-    x = input()
-    valve.setPos(int(x))
+    def goto(self, target):
+        print(self.angle, target)
+        start = time.time()
+        angle = target - self.angle
+        angle %= 360
+        if angle > 180:
+            angle = angle - 360
+        steps = int(self.full * self.gear_ratio * angle / 360)
+        direction = 1
+        GPIO.output(self.dir, HIGH)
+        if steps < 0:
+            direction = -1
+            GPIO.output(self.dir, LOW)
+            steps *= -1
+
+        print(target, self.angle, steps*direction)
+
+        self.interrupt = False
+        self.is_acuating = True
+        for i in range(steps):
+            self._step()
+            self.angle += angle / steps
+            if self.interrupt:
+                print("Interrupting actuation for some reason")
+                break
+        self.priority = -1
+        self.is_acuating = False
+        self.interrupt = False
+        print("Done actuating")
+
+    def actuate(self, target, priority):
+        print(target, priority)
+        if self.aborted:
+            return
+        if self.is_acuating:
+            if priority >= self.priority:
+                print("Interrupting")
+                self.interrupt = True
+                while self.is_acuating:
+                    pass
+                self.priority = priority
+                self.goto(target)
+            else:
+                print("Not actuating, command is lower priority")
+        else:
+            self.priority = priority
+            self.goto(target)
+
+
+if __name__ == '__main__':
+    valve = Valve(4, 17)
+    while 1:
+        x = input()
+        if x == "x":
+            break
+        degree, priority = [int(i) for i in x.split(" ")]
+        actuate_thread = threading.Thread(
+            target=valve.actuate, args=(degree, priority))
+        actuate_thread.daemon = True
+        actuate_thread.start()
+
+    valve.abort()
