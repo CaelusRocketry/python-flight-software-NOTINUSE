3449e6319581651e90f3de09ce426ca8eb52cd67
anna catalano <catalanoanna00@gmail.com>
2019-09-21 18:24:22 -0400

    Edit Kalman filter file

--- modules/sensors/__init__.py
+++ modules/sensors/__init__.py
@@ -4,10 +4,8 @@ from abc import ABC, abstractmethod
 from enum import Enum, IntEnum
 from queue import PriorityQueue
 
-#from filterpy.kalman import KalmanFilter
-#from filterpy.common import Q_discrete_white_noise
-#import numpy as np
-
+from pykalman import KalmanFilter
+import numpy as np
 
 class SensorStatus(IntEnum):
     Safe = 3
@@ -48,16 +46,45 @@ class Sensor(ABC):
     def log(cls) -> PriorityQueue:
         pass
 
-#    def initKalman(self):
-#        self.kf = KalmanFilter( dim_x=2, dim_z=1 )
-#        self.kf.x = np.array([[2.], [0.]])
-#        self.kf.F = np.array([[1.,1.], [0.,1.]])
-#        self.kf.H = np.array([[1.,0.]])
-#        self.kf.P *= 1000.0
-#        self.kf.R = 5
-#        self.kf.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
+    def initKalman(self, training, normalizingFactor): 
+        training = np.asarray(training)
+        initial_state_mean = [training[0], 0] 
+        transition_matrix = [[1, 1], [0, 1]]
+        observation_matrix = [[1, 0]]
 
-#    def updateKalman(self, reading):
-#        self.kf.predict()
-#        self.kf.update(reading)
-#        return self.kf.x
+        kf1 = KalmanFilter(transition_matrices = transition_matrix,
+                observation_matrices = observation_matrix,
+                initial_state_mean = initial_state_mean)
+        kf1 = kf1.em(training, n_iter=5)
+        (smoothed_state_means, smoothed_state_covariances) = kf1.smooth(training)
+
+        self.kf = KalmanFilter(transition_matrices = transition_matrix,
+                        observation_matrices = observation_matrix,
+                        initial_state_mean = initial_state_mean,
+                        observation_covariance = normalizingFactor*kf1.observation_covariance,
+                        em_vars=['transition_covariance', 'initial_state_covariance'])
+
+        self.kf = self.kf.em(training, n_iter=5)
+        (self.filtered_state_means, self.filtered_state_covariances) = self.kf.filter(training)                
+
+    def updateKalman(self, readings):
+        readings = np.asarray(readings)
+        x_now = self.filtered_state_means[-1]
+        P_now = self.filtered_state_covariances[-1]
+        x_new = np.zeros((len(readings), self.filtered_state_means.shape[1]))
+
+
+        # For each new measurement (this is the live update loop)
+        for i, reading in enumerate(readings):
+            # kf.filter_update is basically the predict method
+            (x_now, P_now) = self.kf.filter_update(filtered_state_mean = x_now,
+                                                filtered_state_covariance = P_now,
+                                                observation = reading)
+            # x_now is the normalized data point
+            x_new[i] = x_now
+        
+        return x_new
+
+        
+
+
---
+++ modules/sensors/kalmanTest.py
@@ -0,0 +1,57 @@
+from __init__ import Sensor, SensorStatus, SensorType
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class KalmanTestSensor(Sensor):
+
+    def __init__(self, location):
+        print(location)
+
+    def name(self):
+        return self._name
+
+    def location(self):
+        return self._location
+
+    def status(self):
+        return self._status
+
+    def sensor_type(self):
+        return self._sensor_type
+
+    def log(self):
+        pass
+
+
+sensor = KalmanTestSensor("nowhere")
+training = np.array([399, 403, 409, 416])
+readings = [418, 420, 429, 423, 429, 431, 433, 434, 434, 433, 431, 430, 428, 427, 425, 429, 431, 410, 406, 402, 397, 391, 376, 372, 351, 336, 327, 307]
+    
+sensor.initKalman(training, 150)
+expected_readings = sensor.updateKalman(readings)
+
+readings = np.asarray(readings)
+
+
+plt.figure(3)
+times = range(readings.shape[0])
+old_times = range(training.shape[0])
+new_times = range(readings.shape[0])
+
+plt.plot(times, readings, 'bo')
+plt.plot(new_times, expected_readings[:, 0], 'go')
+plt.plot(new_times, expected_readings[:, 0], 'g--')
+plt.plot(new_times, readings, 'yo')
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
