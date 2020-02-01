92af61f950d903be006afd75d3647e4427d2f6e2
Aarav <akhanna@Khannas-MBP-7.fios-router.home>
2020-02-01 18:53:57 -0500

    Update imu_task.py and telemetry_task.py and Adjust registry.py accordingly

--- flight_software/modules/tasks/imu_task.py
+++ flight_software/modules/tasks/imu_task.py
@@ -1,4 +1,14 @@
 from task import Task
+from abc import ABC, abstractmethod
+from modules.devices.device import Device
+from modules.supervisor.command import Command
+from modules.supervisor.registry import Registry
+from modules.supervisor.flags import Flag
+from modules.drivers.imu_driver import IMU
 
 class ImuTask(Task):
-    pass
+    def read(self, state_field_registry: Registry) -> Registry:
+        updatedDict = IMU.read()
+        for key in updatedDict:
+            Registry.put(key, key.values(), Flag)
+        return Registry
