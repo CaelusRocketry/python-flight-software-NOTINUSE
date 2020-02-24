8aac401ca3790966baad27508e3c526f71bce842
Srikar Gouru <srikarg89@gmail.com>
2020-02-24 16:30:40 -0500

    Remove command.py

--- src/pi/modules/mcl/command.py
+++
@@ -1,9 +0,0 @@
-"""
-Command class that is used to send a command to a device.
-This could just be a dict, but it's more formal to have everything modular (meaning use classes and enums whenever possible)
-"""
-
-class Command:
-
-    def __init__(self):
-        pass
