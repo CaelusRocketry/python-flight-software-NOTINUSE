fe11edf2dcb55b2f48e839c0e692a903e499e9c1
Srikar Gouru <srikarg89@gmail.com>
2020-03-19 15:27:35 -0400

    Edit flag and registry to be more dynamic

--- src/pi/modules/mcl/flag.py
+++ src/pi/modules/mcl/flag.py
@@ -1,6 +1,6 @@
 from modules.lib.mode import Mode
 from modules.lib.errors import AccessError
-from modules.lib.enums import *
+from modules.lib.enums import ValveLocation, ActuationType
 
 class Flag:
 
@@ -13,22 +13,6 @@ class Flag:
             "progress": {
                 "stage": None
             },
-            "sensor_request": {
-                "thermocouple_chamber": None,
-                "thermocouple_tank": None,
-                "pressure_chamber": None,
-                "pressure_tank": None,
-                "pressure_injector": None,
-                "load_tank": None
-            },
-            "valve_request": {
-                "ball_valve_pressure_relief": None,
-                "ball_valve_propellant_vent": None,
-                "ball_valve_main_propellant_valve": None,
-                "solenoid_valve_pressure_relief": None,
-                "solenoid_valve_propellant_vent": None,
-                "solenoid_valve_main_propellant_valve": None
-            },
             "telemetry": {
                 "send_queue": [],
                 "reset": True
@@ -39,34 +23,18 @@ class Flag:
                 "flag_put_error": None,
                 "flag_get_error": None,
             },
-            "valve": {
-                "ball_valve_pres": None,
-                "ball_valve_main": None,
-                "solenoid_valve_drain": None,
-                "solenoid_valve_depres": None,
+            "solenoid": {
+                "actuation_type": {
+                    ValveLocation.PRESSURE_RELIEF: ActuationType.NONE,
+                    ValveLocation.PROPELLANT_VENT: ActuationType.NONE,
+                    ValveLocation.MAIN_PROPELLANT_VALVE: ActuationType.NONE
+                },
+                "actuation_priority": {
+                    ValveLocation.PRESSURE_RELIEF: 0,
+                    ValveLocation.PROPELLANT_VENT: 0,
+                    ValveLocation.MAIN_PROPELLANT_VALVE: 0
+                }
             },
-            "ball_valve_actuate": {
-                "ball_valve_pressure_relief_pulse": None,
-                "ball_valve_propellant_vent_pulse": None,
-                "ball_valve_main_propellant_valve_pulse": None,
-                "ball_valve_pressure_relief_open_vent": None,
-                "ball_valve_propellant_vent_open_vent": None,
-                "ball_valve_main_propellant_valve_open_vent": None,
-                "ball_valve_pressure_relief_close_vent": None,
-                "ball_valve_propellant_vent_close_vent": None,
-                "ball_valve_main_propellant_valve_close_vent": None
-            },
-            "solenoid_valve_actuate": {
-                "solenoid_valve_pressure_relief_pulse": None,
-                "solenoid_valve_propellant_vent_pulse": None,
-                "solenoid_valve_main_propellant_valve_pulse": None,
-                "solenoid_valve_pressure_relief_open_vent": None,
-                "solenoid_valve_propellant_vent_open_vent": None,
-                "solenoid_valve_main_propellant_valve_open_vent": None,
-                "solenoid_valve_pressure_relief_close_vent": None,
-                "solenoid_valve_propellant_vent_close_vent": None,
-                "solenoid_valve_main_propellant_valve_close_vent": None
-            }
         }
 
         self.state_types = {
@@ -76,22 +44,6 @@ class Flag:
             },
             "progress": {
                 "stage": Stage
-            },
-            "sensor_request": {
-                "thermocouple_chamber": bool,
-                "thermocouple_tank": bool,
-                "pressure_chamber": bool,
-                "pressure_tank": bool,
-                "pressure_injector": bool,
-                "load_tank": bool
-            },
-            "valve_request": {
-                "ball_valve_pressure_relief": bool,
-                "ball_valve_propellant_vent": bool,
-                "ball_valve_main_propellant_valve": bool,
-                "solenoid_valve_pressure_relief": bool,
-                "solenoid_valve_propellant_vent": bool,
-                "solenoid_valve_main_propellant_valve": bool
             },
             "telemetry": {
                 "send_queue": list,
@@ -103,34 +55,18 @@ class Flag:
                 "flag_put_error": bool,
                 "flag_get_error": bool,
             },
-            "valve": {
-                "ball_valve_pres": int,
-                "ball_valve_main": int,
-                "solenoid_valve_drain": bool,
-                "solenoid_valve_depres": bool,
+            "solenoid": {
+                "actuation_type": {
+                    ValveLocation.PRESSURE_RELIEF: ActuationType,
+                    ValveLocation.PROPELLANT_VENT: ActuationType,
+                    ValveLocation.MAIN_PROPELLANT_VALVE: ActuationType
+                },
+                "actuation_priority": {
+                    ValveLocation.PRESSURE_RELIEF: int,
+                    ValveLocation.PROPELLANT_VENT: int,
+                    ValveLocation.MAIN_PROPELLANT_VALVE: int
+                }
             },
-            "ball_valve_actuate": {
-                "ball_valve_pressure_relief_pulse": int,
-                "ball_valve_propellant_vent_pulse": int,
-                "ball_valve_main_propellant_valve_pulse": int,
-                "ball_valve_pressure_relief_open_vent": int,
-                "ball_valve_propellant_vent_open_vent": int,
-                "ball_valve_main_propellant_valve_open_vent": int,
-                "ball_valve_pressure_relief_close_vent": int,
-                "ball_valve_propellant_vent_close_vent": int,
-                "ball_valve_main_propellant_valve_close_vent": int
-            },
-            "solenoid_valve_actuate": {
-                "solenoid_valve_pressure_relief_pulse": bool,
-                "solenoid_valve_propellant_vent_pulse": bool,
-                "solenoid_valve_main_propellant_valve_pulse": bool,
-                "solenoid_valve_pressure_relief_open_vent": bool,
-                "solenoid_valve_propellant_vent_open_vent": bool,
-                "solenoid_valve_main_propellant_valve_open_vent": bool,
-                "solenoid_valve_pressure_relief_close_vent": bool,
-                "solenoid_valve_propellant_vent_close_vent": bool,
-                "solenoid_valve_main_propellant_valve_close_vent": bool
-            }
         }
 
     def put(self, key: 'tuple', value) -> AccessError:
--- src/pi/modules/mcl/registry.py
+++ src/pi/modules/mcl/registry.py
@@ -2,6 +2,7 @@ from modules.lib.mode import Mode
 from modules.lib.packet import Packet
 from modules.lib.enums import SensorStatus
 from modules.lib.errors import AccessError
+from modules.lib.enums import SolenoidState, ValveType, ValveLocation, SensorType, SensorLocation
 # from modules.lib.encoding import EnumEncoder
 import time
 import json
@@ -13,16 +14,55 @@ class Registry:
     def __init__(self):
         self.values = {
             "sensor": {
-                "thermocouple": None,
-                "thermocouple_status": SensorStatus.SAFE,
-                "pressure_gas": None,
-                "load_cell_h20": None,
-                "thermocouple_chamber": None,
-                "thermocouple_tank": None,
-                "pressure_chamber": None,
-                "pressure_tank": None,
-                "pressure_injector": None,
-                "load_tank": None
+                SensorType.THERMOCOUPLE: {
+                    SensorLocation.CHAMBER: None,
+                    SensorLocation.TANK: None
+                },
+                SensorType.PRESSURE: {
+                    SensorLocation.CHAMBER: None,
+                    SensorLocation.INJECTOR: None
+                },
+                SensorType.LOAD: {
+                    SensorLocation.TANK: None,
+                    SensorLocation.INJECTOR: None
+                }
+            },
+            "sensor_status": {
+                SensorType.THERMOCOUPLE: {
+                    SensorLocation.CHAMBER: None,
+                    SensorLocation.TANK: None
+                },
+                SensorType.PRESSURE: {
+                    SensorLocation.CHAMBER: None,
+                    SensorLocation.INJECTOR: None
+                },
+                SensorType.LOAD: {
+                    SensorLocation.TANK: None,
+                    SensorLocation.INJECTOR: None
+                }
+            },
+            "valve": {
+                ValveType.SOLENOID: {
+                    ValveLocation.PRESSURE_RELIEF: SolenoidState.CLOSED,
+                    ValveLocation.PROPELLANT_VENT: SolenoidState.CLOSED,
+                    ValveLocation.MAIN_PROPELLANT_VALVE: SolenoidState.CLOSED
+                }
+            },
+            "valve_actuation": {
+                "actuation_type": {
+                    ValveType.SOLENOID: {
+                        ValveLocation.PRESSURE_RELIEF: ActuationType.NONE,
+                        ValveLocation.PROPELLANT_VENT: ActuationType.NONE,
+                        ValveLocation.MAIN_PROPELLANT_VALVE: ActuationType.NONE
+                    }
+                },
+                "actuation_priority": {
+                    ValveType.SOLENOID: {
+                        ValveLocation.PRESSURE_RELIEF: 0,
+                        ValveLocation.PROPELLANT_VENT: 0,
+                        ValveLocation.MAIN_PROPELLANT_VALVE: 0
+                    }
+                }
             },
             "telemetry": {
                 "ingest_queue": [],
@@ -34,16 +74,55 @@ class Registry:
         }
         self.types = {
             "sensor": {
-                "thermocouple": float,
-                "thermocouple_status": SensorStatus,
-                "pressure_gas": float,
-                "load_cell_h20": float,
-                "thermocouple_chamber": float,
-                "thermocouple_tank": float,
-                "pressure_chamber": float,
-                "pressure_tank": float,
-                "pressure_injector": float,
-                "load_tank": float,
+                SensorType.THERMOCOUPLE: {
+                    SensorLocation.CHAMBER: (float, SensorStatus),
+                    SensorLocation.TANK: (float, SensorStatus)
+                },
+                SensorType.PRESSURE: {
+                    SensorLocation.CHAMBER: (float, SensorStatus),
+                    SensorLocation.INJECTOR: (float, SensorStatus)
+                },
+                SensorType.LOAD: {
+                    SensorLocation.TANK: (float, SensorStatus),
+                    SensorLocation.INJECTOR: (float, SensorStatus)
+                }
+            },
+            "sensor_status": {
+                SensorType.THERMOCOUPLE: {
+                    SensorLocation.CHAMBER: SensorStatus,
+                    SensorLocation.TANK: SensorStatus
+                },
+                SensorType.PRESSURE: {
+                    SensorLocation.CHAMBER: SensorStatus,
+                    SensorLocation.INJECTOR: SensorStatus
+                },
+                SensorType.LOAD: {
+                    SensorLocation.TANK: SensorStatus,
+                    SensorLocation.INJECTOR: SensorStatus
+                }
+            },
+            "valve": {
+                ValveType.SOLENOID: {
+                    ValveLocation.PRESSURE_RELIEF: SolenoidState,
+                    ValveLocation.PROPELLANT_VENT: SolenoidState,
+                    ValveLocation.MAIN_PROPELLANT_VALVE: SolenoidState
+                },
+            },
+            "valve_actuation": {
+                "actuation_type": {
+                    ValveType.SOLENOID: {
+                        ValveLocation.PRESSURE_RELIEF: ActuationType,
+                        ValveLocation.PROPELLANT_VENT: ActuationType,
+                        ValveLocation.MAIN_PROPELLANT_VALVE: ActuationType
+                    }
+                },
+                "actuation_priority": {
+                    ValveType.SOLENOID: {
+                        ValveLocation.PRESSURE_RELIEF: int,
+                        ValveLocation.PROPELLANT_VENT: int,
+                        ValveLocation.MAIN_PROPELLANT_VALVE: int
+                    }
+                }
             },
             "telemetry": {
                 "ingest_queue": list,
@@ -56,25 +135,30 @@ class Registry:
         }
         self.times = {i: {j: None for j in i} for i in self.values}
 
-    def put(self, key: 'tuple', value) -> AccessError:
-        outer, inner = key
-        if outer not in self.values:
-            return AccessError.PUT_ERROR
-        if inner not in self.values[outer]:
-            return AccessError.PUT_ERROR
-        if not isinstance(value, self.types[outer][inner]):
+    def put(self, path: 'tuple', value) -> AccessError:
+        values, types, times = self.values, self.types, self.times
+        key = path[-1]
+        path = path[:-1]
+        for p in path:
+            if p not in values:
+                return AccessError.KEY_ERROR
+            values = values[p]
+            types = types[p]
+            times = times[p]
+        if not isinstance(values[key], types[key]):
             return AccessError.KEY_ERROR
-        self.values[outer][inner] = value
-        self.times[outer][inner] = time.time()
+        values[key] = value
+        times[key] = time.time()
         return AccessError.NONE
 
-    def get(self, key):
-        outer, inner = key
-        if outer not in self.values:
-            return AccessError.KEY_ERROR
-        if inner not in self.values[outer]:
-            return AccessError.KEY_ERROR
-        return self.values[outer][inner]
+    def get(self, path) -> tuple:
+        values, times = self.values, self.times
+        for p in path:
+            if p not in values:
+                return AccessError.KEY_ERROR, None, None
+            values = values[p]
+            times = times[p]
+        return AccessError.NONE, values, times
 
     def to_string(self):
         return json.dumps(self.values, cls=EnumEncoder)
