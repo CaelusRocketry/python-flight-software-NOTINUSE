99f67608931f119dc2565b2132f4096908d7a3ea
adiv413 <hsfan612@gmail.com>
2020-04-20 18:02:27 -0400

    Make Registry read from json

--- include/flight/modules/lib/Enums.hpp
+++ include/flight/modules/lib/Enums.hpp
@@ -5,66 +5,64 @@
 #ifndef FLIGHT_ENUMS_HPP
 #define FLIGHT_ENUMS_HPP
 
-class Enums {
-    //map enums to strings: https://stackoverflow.com/a/24296298/11043216
+//map enums to strings: https://stackoverflow.com/a/24296298/11043216
 
-    enum class SensorType {
-        THERMOCOUPLE,
-        PRESSURE,
-        LOAD
-    };
+enum class SensorType {
+    THERMOCOUPLE,
+    PRESSURE,
+    LOAD
+};
 
-    enum class SensorLocation {
-        CHAMBER,
-        TANK,
-        INJECTOR
-    };
+enum class SensorLocation {
+    CHAMBER,
+    TANK,
+    INJECTOR
+};
 
-    enum class SolenoidState {
-        OPEN,
-        CLOSED
-    };
+enum class SolenoidState {
+    OPEN,
+    CLOSED
+};
 
-    enum class SensorStatus {
-        SAFE = 3,
-        WARNING = 2,
-        CRITICAL = 1
-    };
+enum class SensorStatus {
+    SAFE = 3,
+    WARNING = 2,
+    CRITICAL = 1
+};
 
-    enum class ValveType {
-        SOLENOID,
-        BALL
-    };
+enum class ValveType {
+    SOLENOID,
+    BALL
+};
 
-    enum class ValveLocation {
-        PRESSURE_RELIEF,
-        PROPELLANT_VENT,
-        MAIN_PROPELLANT_VALVE
-    };
+enum class ValveLocation {
+    PRESSURE_RELIEF,
+    PROPELLANT_VENT,
+    MAIN_PROPELLANT_VALVE
+};
 
-    enum class ActuationType {
-        PULSE,
-        OPEN_VENT,
-        CLOSE_VENT,
-        NONE
-    };
+enum class ActuationType {
+    PULSE,
+    OPEN_VENT,
+    CLOSE_VENT,
+    NONE
+};
 
-    enum class ValvePriority {
-        NONE = 0,
-        LOW_PRIORITY = 1,
-        PI_PRIORITY = 2,
-        MAX_TELEMETRY_PRIORITY = 3,
-        ABORT_PRIORITY = 4
-    };
+enum class ValvePriority {
+    NONE = 0,
+    LOW_PRIORITY = 1,
+    PI_PRIORITY = 2,
+    MAX_TELEMETRY_PRIORITY = 3,
+    ABORT_PRIORITY = 4
+};
 
-    enum class Stage {
-        PROPELLANT_LOADING,
-        LEAK_TESTING_1,
-        PRESSURANT_LOADING,
-        LEAK_TESTING_2,
-        PRE_IGNITION,
-        DISCONNECTION
-    };
+enum class Stage {
+    PROPELLANT_LOADING,
+    LEAK_TESTING_1,
+    PRESSURANT_LOADING,
+    LEAK_TESTING_2,
+    PRE_IGNITION,
+    DISCONNECTION
 };
 
 #endif //FLIGHT_ENUMS_HPP
--- src/modules/mcl/Registry.cpp
+++ src/modules/mcl/Registry.cpp
@@ -1,21 +1,38 @@
 #include <queue>
 #include <flight/modules/mcl/Registry.hpp>
 #include <flight/modules/mcl/Field.hpp>
+#include <flight/modules/lib/Enums.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+namespace pt = boost::property_tree;
 
 Registry::Registry(){
     log("Registry created");
 
-    //TODO: convert this to parsing from json: https://www.codespeedy.com/read-data-from-json-file-in-cpp/
+    //parsing from json: https://www.codespeedy.com/read-data-from-json-file-in-cpp/
+
+    pt::ptree root;
+    pt::read_json("flight/modules/lib/config.json", root);
 
     // Sensor fields
-    add<double>("sensor_measured.thermocouple.chamber", 0.0);
-    add<double>("sensor_measured.thermocouple.tank", 0.0);
-    add<double>("sensor_measured.pressure.chamber", 0.0);
-    add<double>("sensor_measured.pressure.tank", 0.0);
-    add<double>("sensor_measured.pressure.injector", 0.0);
-    add<double>("sensor_measured.load.tank", 0.0);
+    auto sensor = root.get_child("sensors").get_child("list");
+    for(auto &outer : sensor) {
+        for(auto &inner : sensor.get_child(outer.first)) {
+            add<double>("sensor_measured." + outer.first + "." + inner.second.get_value<std::string>(), 0.0);
+            add<double>("sensor_normalized." + outer.first + "." + inner.second.get_value<std::string>(), 0.0);
+            add<SensorStatus>("sensor_status." + outer.first + "." + inner.second.get_value<std::string>(), SensorStatus::SAFE);
+        }
+    }
 
     // Valve fields
+    auto valve = root.get_child("valves").get_child("list");
+    for(auto &outer : sensor) {
+        for(auto &inner : sensor.get_child(outer.first)) {
+            add<SolenoidState>("valve." + outer.first + "." + inner.second.get_value<std::string>(), SolenoidState::CLOSED);
+            add<ActuationType>("valve_actuation." + outer.first + "." + inner.second.get_value<std::string>(), ActuationType::NONE);
+        }
+    }
 
     // Telemetry fields
     add<priority_queue<int>>("telemetry.ingest_queue");
@@ -25,7 +42,7 @@ Registry::Registry(){
     // General fields
     add<bool>("general.hard_abort", false);
     add<bool>("general.soft_abort", false);
-    add<int>("general.stage", 0);
+    add<Stage>("general.stage", Stage::PROPELLANT_LOADING);
     add<int>("general.stage_progress", 0);
 }
 
