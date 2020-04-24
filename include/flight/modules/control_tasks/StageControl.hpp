2739ee5dea8526245ac5da71bf605b11a05acfe9
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-04-24 03:23:20 -0400

    Add header files for valve and stage control

--- include/flight/modules/control_tasks/StageControl.hpp
+++ include/flight/modules/control_tasks/StageControl.hpp
@@ -5,4 +5,27 @@
 #ifndef FLIGHT_STAGECONTROL_HPP
 #define FLIGHT_STAGECONTROL_HPP
 
+#include <flight/modules/mcl/Registry.hpp>
+#include <flight/modules/mcl/Flag.hpp>
+
+class StageControl {
+private:
+    Registry registry;
+    Flag flag;
+    double m_requestTime;
+    double m_sendTime;
+    double m_startTime;
+
+    double calculateStatus();
+
+    void sendProgressionRequest();
+    void sendData();
+    void progress();
+    void stageValveControl();
+
+public:
+    StageControl(Registry, Flag);
+    void execute();
+};
+
 #endif //FLIGHT_STAGECONTROL_HPP
--- include/flight/modules/control_tasks/ValveControl.hpp
+++ include/flight/modules/control_tasks/ValveControl.hpp
@@ -5,4 +5,27 @@
 #ifndef FLIGHT_VALVECONTROL_HPP
 #define FLIGHT_VALVECONTROL_HPP
 
+#include <flight/modules/mcl/Registry.hpp>
+#include <flight/modules/mcl/Flag.hpp>
+#include <vector>
+#include <string>
+
+class ValveControl {
+private:
+    Registry registry;
+    Flag flag;
+    std::vector<std::string> valves;
+    double m_sendInterval;
+    double m_lastSendTime;
+
+    void sendValveData();
+    void abort();
+    void undoAbort();
+    void checkAbort();
+
+public:
+    ValveControl(Registry, Flag);
+    void execute();
+};
+
 #endif //FLIGHT_VALVECONTROL_HPP
