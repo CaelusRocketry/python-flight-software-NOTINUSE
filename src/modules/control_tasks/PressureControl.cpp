14de79a059f0672534fd49499fce954b5ef26b86
Aditya Vasantharao <hsfan612@gmail.com>
2020-06-27 13:23:15 -0400

    Fix PressureControl

--- src/modules/control_tasks/PressureControl.cpp
+++ src/modules/control_tasks/PressureControl.cpp
@@ -19,14 +19,12 @@ void PressureControl::execute() {
 }
 
 void PressureControl::check_pressure() {
-    //TODO: make this actually do something
-    if(int(this->registry->get<SensorStatus>("sensor_status.pressure.chamber")) == int(SensorStatus::CRITICAL)) {
-        //actuate specific valves
-    }
-    if(int(this->registry->get<SensorStatus>("sensor_status.pressure.tank")) == int(SensorStatus::CRITICAL)) {
-        //actuate specific valves
-    }
-    if(int(this->registry->get<SensorStatus>("sensor_status.pressure.injector")) == int(SensorStatus::CRITICAL)) {
-        //actuate specific valves
+    //TODO: make sure that the pressure relief valve is the only thing that has to be actuated in this scenario
+    for(string &loc : Util::parse_json_list({"sensors", "list", "pressure"})) {
+        if(int(this->registry->get<SensorStatus>("sensor_status.pressure." + loc)) == int(SensorStatus::CRITICAL)) {
+            Util::enqueue(this->flag, Log("response", "{\"header\": \"info\", \"Description\": \"Pressure in " + loc + " is too high; actuating pressure relief valve.\"}"), LogPriority::CRIT);
+            this->flag->put("valve_actuation_type.solenoid.pressure_relief", ActuationType::OPEN_VENT);
+            this->flag->put("valve_actuation_priority.solenoid.pressure_relief", ValvePriority::MAX_TELEMETRY_PRIORITY);
+        }
     }
 }
