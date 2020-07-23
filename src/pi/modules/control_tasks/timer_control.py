0a05901a0e3c686d2453ceeac6c0e21bb4b80867
Rashed Hossain <nurdinhossain@gmail.com>
2020-07-22 20:55:24 -0400

    Create TimerControl

---
+++ src/pi/modules/control_tasks/timer_control.py
@@ -0,0 +1,20 @@
+# The only point of this class is to slow down the code while testing
+import time
+
+class TimerControl:
+
+    def __init__(self, registry, flag):
+        pass
+
+    def begin(self, config: dict):
+        self.config = config
+        self.delay = self.config["timer"]["delay"]
+
+    def busy_wait(self, target):  
+        current_time = time.time()
+        while (time.time() <= current_time + target):
+            print("Waiting")
+        print("Finished")
+
+    def execute(self):
+        self.busy_wait(self.delay)
--- src/pi/valve_test.py
+++ src/pi/valve_test.py
@@ -2,7 +2,7 @@ import json
 from modules.mcl.supervisor import Supervisor
 
 config = json.loads(open("config.json").read())
-controls = ["telemetry", "valve", "slow"]
+controls = ["telemetry", "valve", "timer"]
 tasks = ["telemetry", "valve"]
 task_config = {"tasks": tasks, "control_tasks": controls}
 supervisor = Supervisor(task_config=task_config, config=config)
