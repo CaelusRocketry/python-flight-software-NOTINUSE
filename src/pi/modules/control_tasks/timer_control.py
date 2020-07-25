7bcb5f71e98a24d024069cf4cb1cb6c1c84ec7c8
Rashed Hossain <nurdinhossain@gmail.com>
2020-07-25 13:16:59 -0400

    .

--- src/pi/modules/control_tasks/timer_control.py
+++ src/pi/modules/control_tasks/timer_control.py
@@ -18,14 +18,8 @@ class TimerControl:
 
         start_time = self.registry.get(("general", "mcl_start_time"))
         current_time = time.time()
-<<<<<<< HEAD
-        while (time.time() <= current_time + target):
-            pass
-        print(time.time() - current_time)
-=======
         
         # if mcl is finished early, delay it so that it reaches the minimum run time
->>>>>>> 0f52dbba9a66ec8a763fc21f593a36f55de490e3
 
         if current_time < start_time + self.min_mcl_run_time:
             time.delay(start_time + self.min_mcl_run_time - current_time)
