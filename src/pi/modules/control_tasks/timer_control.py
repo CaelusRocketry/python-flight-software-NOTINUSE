5d4ce4554aa5cb3c450dd7a587c6056da1f9e6b2
Rashed Hossain <nurdinhossain@gmail.com>
2020-07-25 13:25:09 -0400

    Fix timer_control

--- src/pi/modules/control_tasks/timer_control.py
+++ src/pi/modules/control_tasks/timer_control.py
@@ -1,5 +1,5 @@
 # The only point of this class is to slow down the code while testing
-from modules.mcl.registry import Flag
+from modules.mcl.flag import Flag
 from modules.mcl.registry import Registry
 import time
 
@@ -16,10 +16,11 @@ class TimerControl:
     def execute(self):
         # all values, including min_mcl_run_time, are in seconds
 
-        start_time = self.registry.get(("general", "mcl_start_time"))
+        start_time = self.registry.get(("general", "mcl_start_time"))[-1]
         current_time = time.time()
         
         # if mcl is finished early, delay it so that it reaches the minimum run time
 
         if current_time < start_time + self.min_mcl_run_time:
-            time.delay(start_time + self.min_mcl_run_time - current_time)
+            time.sleep(start_time + self.min_mcl_run_time - current_time)
+        print(time.time() - current_time)
