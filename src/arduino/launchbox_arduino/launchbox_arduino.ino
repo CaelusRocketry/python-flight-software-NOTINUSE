7eb1a98967e802eb493cbea202649f22fbed585d
Aditya Vasantharao <hsfan612@gmail.com>
2020-08-16 13:33:54 -0400

    Update launch box code to work with 3 state switches

--- src/arduino/launchbox_arduino/launchbox_arduino.ino
+++ src/arduino/launchbox_arduino/launchbox_arduino.ino
@@ -1,5 +1,5 @@
 /*
-toggle - on/off switch
+toggle - on/off/on switch
 pulse - you push it and it opens, waits .05s, and closes
 
 Nitrogen Fill (1 switch (toggle)))
@@ -13,19 +13,19 @@ Nitrous Oxide Vent
 Nitrous Oxide Main Propellant Valve
 */
 
-// Pin definitions
+// Pin definitions - each toggle switch takes up 2 pins, the pulse pins take up one pin each
 #define NITROGEN_FILL 2
-#define ETHANOL_DRAIN 3
-#define ETHANOL_VENT 4
-#define ETHANOL_VENT_PULSE 5
-#define ETHANOL_MPV 6
-#define NO_FILL 7
-#define NO_DRAIN 8
-#define NO_VENT 9
-#define NO_VENT_PULSE 10
-#define NO_MPV 11
-#define ABORT_PIN 12
-#define OVERRIDE_PIN 13
+#define ETHANOL_DRAIN 4
+#define ETHANOL_VENT 6
+#define ETHANOL_MPV 8
+#define NO_FILL 10
+#define NO_DRAIN 12
+#define NO_VENT 14
+#define NO_MPV 16
+#define ETHANOL_VENT_PULSE 18
+#define NO_VENT_PULSE 19
+#define ABORT_PIN 20
+#define OVERRIDE_PIN 21
 
 // Valve arduino pin definitions - MAKE SURE THESE MATCH VALVE ARDUINO
 #define VALVE_NITROGEN_FILL 2
@@ -38,55 +38,55 @@ Nitrous Oxide Main Propellant Valve
 #define VALVE_NO_MPV 9
 
 // Serial definitions
-const DATA = 0;
-const CLOSE_VENT = 1;
-const OPEN_VENT = 2;
-const PULSE = 3;
-const OVERRIDE = 0;
-const OVERRIDE_UNDO = 1;
+const int DATA = 0;
+const int CLOSE_VENT = 1;
+const int OPEN_VENT = 2;
+const int PULSE = 3;
+const int OVERRIDE = 0;
+const int OVERRIDE_UNDO = 1;
 
 // Pin order and actuation types
-const MAX_PIN = 14;
-int PIN_MAP[] = new int[MAX_PIN];
-PIN_MAP[NITROGEN_FILL] = VALVE_NITROGEN_FILL;
-PIN_MAP[ETHANOL_DRAIN] = VALVE_ETHANOL_DRAIN;
-PIN_MAP[ETHANOL_VENT] = VALVE_ETHANOL_VENT;
-PIN_MAP[ETHANOL_VENT_PULSE] = VALVE_ETHANOL_VENT;
-PIN_MAP[ETHANOL_MPV] = VALVE_ETHANOL_MPV;
-PIN_MAP[NO_FILL] = VALVE_NO_FILL;
-PIN_MAP[NO_DRAIN] = VALVE_NO_DRAIN;
-PIN_MAP[NO_VENT] = VALVE_NO_VENT;
-PIN_MAP[NO_VENT_PULSE] = VALVE_NO_VENT;
-PIN_MAP[NO_MPV] = VALVE_NO_MPV;
+const int DO_NOTHING = 0;
+const int MAX_PIN = 21;
+int PIN_MAP[MAX_PIN];
 
 // Pin counts
-const NUM_VALVES = 8;
-const NUM_BUTTONS = 2;
+const int NUM_VALVES = 8;
+const int NUM_BUTTONS = 2;
 
 // Local variables
 boolean override;
 boolean aborted;
 
 // Arrays that keep track of stuff
-// States: 0 means closed, 1 means open -> should match digitalRead
+// States: 0 means do nothing, 1 is CLOSE_VENT, and 2 is OPEN_VENT (matches the constants)
 // TODO: CHECK IF digitalRead CAN BE COMPARED TO A BOOLEAN
-boolean states[] = new boolean[NUM_VALVES];
-boolean pulsing[] = new boolean[NUM_BUTTONS];
-int vent_pins[] = new int[NUM_VALVES];
-int pulse_pins[] = new int[NUM_BUTTONS];
+int states[NUM_VALVES];
+boolean pulsing[NUM_BUTTONS];
+int vent_pins[] = {NITROGEN_FILL, ETHANOL_DRAIN, ETHANOL_VENT, ETHANOL_MPV, NO_FILL, NO_DRAIN, NO_VENT, NO_MPV};
+int pulse_pins[] = {ETHANOL_VENT_PULSE, NO_VENT_PULSE};
 
 void setup(){
-    for(int i = 2; i <= 13; i++){
-        pinMode(i, INPUT);
+    PIN_MAP[NITROGEN_FILL] = VALVE_NITROGEN_FILL;
+    PIN_MAP[ETHANOL_DRAIN] = VALVE_ETHANOL_DRAIN;
+    PIN_MAP[ETHANOL_VENT] = VALVE_ETHANOL_VENT;
+    PIN_MAP[ETHANOL_VENT_PULSE] = VALVE_ETHANOL_VENT;
+    PIN_MAP[ETHANOL_MPV] = VALVE_ETHANOL_MPV;
+    PIN_MAP[NO_FILL] = VALVE_NO_FILL;
+    PIN_MAP[NO_DRAIN] = VALVE_NO_DRAIN;
+    PIN_MAP[NO_VENT] = VALVE_NO_VENT;
+    PIN_MAP[NO_VENT_PULSE] = VALVE_NO_VENT;
+    PIN_MAP[NO_MPV] = VALVE_NO_MPV;
+
+    for(int i = 2; i <= 17; i++){
+        pinMode(i, INPUT_PULLUP);
     }
     Serial.begin(9600);
     override = false;
     aborted = false;
-
-    vent_pins[] = {NITROGEN_FILL, ETHANOL_DRAIN, ETHANOL_VENT, ETHANOL_MPV, NO_FILL, NO_DRAIN, NO_VENT, NO_MPV};
-    pulse_pins[] = {ETHANOL_VENT_PULSE, NO_VENT_PULSE};
+    
     for(int i = 0; i < NUM_VALVES; i++){
-        states[i] = 0;
+        states[i] = DO_NOTHING; 
     }
     for(int i = 0; i < NUM_BUTTONS; i++){
         pulsing[i] = false;
@@ -98,14 +98,10 @@ void loop(){
         return;
     }
     for(int i = 0; i < NUM_VALVES; i++){
-        if(digitalRead(vent_pins[i]) != states[i]){
-            states[i] = !states[i];
-            if(states[i]){
-                send_message(OPEN_VENT, PIN_MAP[vent_pins[i]]);
-            }
-            else{
-                send_message(CLOSE_VENT, closed[i]);
-            }
+        int current_state = checkToggleSwitch((i + 1) * 2); // (i + 1) * 2 maps from array index to pin number
+        if(current_state != states[i]){
+            states[i] = current_state;
+            send_message(current_state, PIN_MAP[vent_pins[i]]);
         }
     }
     if(digitalRead(OVERRIDE_PIN) && !override){
@@ -144,4 +140,40 @@ void send_message(int cmd, int data){
     Serial.write(cmd);
     Serial.write(data);
     delay(50);
+}
+
+/*
+ * note: these SPDT switches all have their common pins connected to GND because arduino only has an INPUT_PULLUP option instead of an INPUT_PULLDOWN option
+ * this means that a value of LOW indicates that the switch has been flicked in a certain direction, not HIGH
+ * switchStart always maps to CLOSE_VENT, OFF maps to DO_NOTHING, switchStart + 1 maps to OPEN_VENT
+
+ * @param switchStart: the first pin of a toggle switch 
+ * @return CLOSE_VENT if the switch is closest to switchStart, DO_NOTHING if it's off, and OPEN_VENT if it's closest to switchStart + 1
+ 
+ * for example, if this is the switch:
+ *                 /
+ *                /
+ *               /
+ *              /
+ *             /
+ *  ----------/-----------
+ *  8 (ON)  (OFF)        9 (ON)
+ *  CL_VNT  DO_NTHING    OPEN_VENT
+
+ * then startSwitch is 8 and the method returns OPEN_VENT
+ * note that in this scenario, OPEN_VENT is returned even though switchStart has a value of LOW
+ * this reversal has to be done (i.e. we can't just check for HIGH) because the OFF state always has a state of HIGH, which means that 2 pins are always HIGH, and 
+ * because of how SPDT works, the opposite pin is LOW
+*/
+
+int checkToggleSwitch(int switchStart) {
+  if(digitalRead(switchStart) == LOW) {
+    return OPEN_VENT;  
+  }  
+  else if(digitalRead(switchStart + 1) == LOW) {
+    return CLOSE_VENT;
+  }
+  else {
+    return DO_NOTHING;
+  }
 }
